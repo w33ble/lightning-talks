d256c5d893efc097535cde228ff25b3479e6bf57
Joe Fleming <joe.fleming@gmail.com>
2017-04-12 09:45:28 -0700

    for loop default in evens example

--- wrangling-arrays/01-evens.js
+++ wrangling-arrays/01-evens.js
@@ -6,5 +6,13 @@ checking for x % 2 === 0 is a good way
 
 */
 
+const output = require('./lib/output');
 const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+const evens = [];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 === 0) evens.push(arr[i]);
+}
+
+output(evens);
