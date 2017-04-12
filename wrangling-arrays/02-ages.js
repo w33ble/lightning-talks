e3c2799a025de7dcb0a5c6cde2a9fe59ba8d934f
Joe Fleming <joe.fleming@gmail.com>
2017-04-11 19:09:57 -0700

    add user avg age example

--- wrangling-arrays/02-ages.js
+++ wrangling-arrays/02-ages.js
@@ -19,4 +19,15 @@ example user object:
 const output = require('./lib/output');
 const users = require('./users.json');
 
+// show the users data
 output(users);
+
+// for loop to calculate ages
+// let sumAges = 0;
+
+// for (let i = 0; i < users.length; i++) {
+//   const { age } = users[i];
+//   sumAges += age;
+// }
+
+// output({ averageAge: sumAges / users.length })
